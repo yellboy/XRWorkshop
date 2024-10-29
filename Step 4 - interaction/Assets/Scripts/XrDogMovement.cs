232579f230121f3d621b12b2b46885d8e9b72b75
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2024-10-29 14:34:29 +0100

    basic interaction

---
+++ Step 4 - interaction/Assets/Scripts/XrDogMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class XrDogMovement : MonoBehaviour
+    {
+        private const float SpeedUnitsPerSecond = 1f;
+
+        private bool _movementInProgress;
+        private Vector3 _targetPosition;
+        private float _startTime;
+        private Vector3 _startPosition;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_movementInProgress)
+            {
+                var elapsedTime = Time.time - _startTime;
+                var distanceCovered = elapsedTime * SpeedUnitsPerSecond;
+                gameObject.transform.position = Vector3.Lerp(_startPosition, _targetPosition, distanceCovered);
+
+                if (_targetPosition == gameObject.transform.position)
+                {
+                    _movementInProgress = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+            {
+                _movementInProgress = true;
+                _startTime = Time.time;
+
+                _startPosition = gameObject.transform.position;
+                _targetPosition = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+
+            if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+            {
+                _movementInProgress = true;
+                _startTime = Time.time;
+
+                _startPosition = gameObject.transform.position;
+                _targetPosition = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+        }
+    }
+}

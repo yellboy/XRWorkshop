db41baacedba07853da5f19d3726f4f4ef513c6d
Sasa <sasa.trajkova@gmail.com>
2024-11-04 12:21:47 +0100

    implement Sasa's movement

--- Step 3 - XR app/Assets/Scripts/DogMovement.cs
+++ Step 3 - XR app/Assets/Scripts/DogMovement.cs
@@ -1,58 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class SimpleDogMovement : MonoBehaviour
 {
-    public class DogMovement : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        private const float SpeedUnitsPerSecond = 1f;
 
-        private bool _movementInProgress;
-        private Vector3 _targetPosition;
-        private float _startTime;
-        private Vector3 _startPosition;
+    }
 
-        // Start is called before the first frame update
-        void Start()
+    private const float SpeedUnitsPerSecond = 3f;
+    private bool _movementInProgress;
+    private Vector3 _targetPosition;
+    private float _startTime;
+    private Vector3 _startPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_movementInProgress)
         {
-        
+            var elapsedTime = Time.time - _startTime;
+            var distanceCovered = elapsedTime * SpeedUnitsPerSecond;
+            gameObject.transform.position = Vector3.Lerp(_startPosition, _targetPosition, distanceCovered);
+
+            if (_targetPosition == gameObject.transform.position)
+            {
+                _movementInProgress = false;
+            }
+            else
+            {
+                return;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        _startPosition = this.gameObject.transform.position;
+        var newPosition = _startPosition;
+
+        if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
         {
-            if (_movementInProgress)
-            {
-                var elapsedTime = Time.time - _startTime;
-                var distanceCovered = elapsedTime * SpeedUnitsPerSecond;
-                gameObject.transform.position = Vector3.Lerp(_startPosition, _targetPosition, distanceCovered);
-
-                if (_targetPosition == gameObject.transform.position)
-                {
-                    _movementInProgress = false;
-                }
-                else
-                {
-                    return;
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                _movementInProgress = true;
-                _startTime = Time.time;
-
-                _startPosition = gameObject.transform.position;
-                _targetPosition = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
-            }
-
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                _movementInProgress = true;
-                _startTime = Time.time;
-
-                _startPosition = gameObject.transform.position;
-                _targetPosition = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
-            }
+            _movementInProgress = true;
+            _startTime = Time.time;
+            _targetPosition = UpdateXPosition(-1, _startPosition);
         }
+
+        if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        {
+            _movementInProgress = true;
+            _startTime = Time.time;
+            _targetPosition = UpdateXPosition(1, _startPosition);
+        }
+
+        if (OVRInput.Get(OVRInput.RawButton.LHandTrigger))
+        {
+            _movementInProgress = true;
+            _startTime = Time.time;
+            _targetPosition = UpdateZPosition(1, _startPosition);
+        }
+
+        if (OVRInput.Get(OVRInput.RawButton.RHandTrigger))
+        {
+            _movementInProgress = true;
+            _startTime = Time.time;
+            _targetPosition = UpdateZPosition(-1, _startPosition);
+        }
+    }
+
+    private Vector3 UpdateXPosition(int delta, Vector3 activePosition)
+    {
+        return new Vector3(activePosition.x + delta, activePosition.y, activePosition.z);
+    }
+
+    private Vector3 UpdateZPosition(int delta, Vector3 activePosition)
+    {
+        return new Vector3(activePosition.x, activePosition.y, activePosition.z + delta);
     }
 }
